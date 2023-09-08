df4e546ff49285ca2063f37d4a9b3e3749233be0
Aram Valcourt <115132116+AramCodes@users.noreply.github.com>
2023-09-08 04:52:42 -0400

    original setup and side bar component made

---
+++ components/SideMenu.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+export default function SideMenu() { 
+    return (
+        <section id="sideMenu">
+            <nav>
+                <a href="#" className="active"> 
+                <i className="fa-regular fa-paper-plane"></i>
+                Send Invoice
+                </a>
+                
+                <a href="#">
+                <i className="fa-solid fa-database"></i>
+                GET Invoices
+                </a>
+                
+                <a href="#">
+                <i className="fa-solid fa-server"></i>
+                POST an Invoice
+                </a>
+                
+                <a href="#">
+                <i className="fa-solid fa-upload"></i>
+                PUT an Invoice
+                </a>
+                
+                <a href="#">
+                <i className="fa-solid fa-server fa-fade"></i>
+                PATCH an Invoice
+                </a>
+            </nav>
+        </section>
+    )
+}
